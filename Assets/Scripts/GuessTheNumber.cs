8f7f373eee5f6cdb4a132bdf8a4a5fa57c3f5ac1
Alastair <alas_lair@proton.me>
2025-02-07 00:39:49 -0600

    feat: add GameSetup method

--- Assets/Scripts/GuessTheNumber.cs
+++ Assets/Scripts/GuessTheNumber.cs
@@ -5,6 +5,7 @@ public class GuessTheNumber : MonoBehaviour
 {
     [SerializeField] TMP_Text header;
     [SerializeField] TMP_InputField guessInputField;
+    int numberToGuess;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -16,5 +17,12 @@ public class GuessTheNumber : MonoBehaviour
     void Update()
     {
         
+    }
+
+    void GameSetup()
+    {
+        numberToGuess = Random.Range(int.MinValue, int.MaxValue);
+        header.text = "I'm thinking of a number between 1 and 10. You have 3 attempts to guess it...";
+        guessInputField.text = "";
     }
 }
