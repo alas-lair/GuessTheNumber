80fbfcbb1fd7bad3a01247163a20e3906c3ce301
Alastair <alas_lair@proton.me>
2025-02-07 00:43:38 -0600

    feat: add GameSetup() call to both Start() and the Reset button

--- Assets/Scripts/GuessTheNumber.cs
+++ Assets/Scripts/GuessTheNumber.cs
@@ -11,7 +11,7 @@ public class GuessTheNumber : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        GameSetup();
     }
 
     // Update is called once per frame
