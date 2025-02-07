03fbe1ac9005699f22bd5f8961725451deabc483
Alastair <alas_lair@proton.me>
2025-02-07 00:59:32 -0600

    feat: add SubmitGuess method

--- Assets/Scripts/GuessTheNumber.cs
+++ Assets/Scripts/GuessTheNumber.cs
@@ -1,10 +1,12 @@
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class GuessTheNumber : MonoBehaviour
 {
     [SerializeField] TMP_Text header;
     [SerializeField] TMP_InputField guessInputField;
+    [SerializeField] Button submitButton;
     int numberToGuess;
     int attempts;
 
@@ -26,5 +28,39 @@ public class GuessTheNumber : MonoBehaviour
         numberToGuess = Random.Range(1, 10 + 1);
         header.text = $"I'm thinking of a number between 1 and 10. You have {attempts} attempts to guess it...";
         guessInputField.text = "";
+        guessInputField.enabled = true;
+        submitButton.enabled = true;
+    }
+
+    public void SubmitGuess()
+    {
+        // Input validation
+        if (guessInputField.text.Trim().Length == 0)
+        {
+            header.text = "Please type in a number.";
+            return;
+        }
+        // Win condition
+        if (numberToGuess == int.Parse(guessInputField.text))
+        {
+            header.text = $"You got it right with {attempts} attempts left! The number was {numberToGuess}.";
+            guessInputField.enabled = false;
+            submitButton.enabled = false;
+        }
+        else
+        {
+            attempts--;
+            // Lose condition
+            if (attempts == 0)
+            {
+                header.text = $"Game over. The number was {numberToGuess}.";
+                guessInputField.enabled = false;
+                submitButton.enabled = false;
+            }
+            else
+            {
+                header.text = $"Nope! You have {attempts} attempts left! Try again.";
+            }
+        }
     }
 }
